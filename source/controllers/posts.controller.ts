aa3391aa75a1c63e6ac37217294b97064378734a
MDOsipov <dos12345671@gmail.com>
2022-10-22 18:38:46 +0300

    Update and delete operations are added

--- source/controllers/posts.controller.ts
+++ source/controllers/posts.controller.ts
@@ -22,4 +22,44 @@ async function addPost(req: Request, res: Response, next: NextFunction) {
     });
 }
 
-export default { getPosts, addPost };
+async function updatePostByOwner(req: Request, res: Response, next: NextFunction) {
+    const owner: string = req.body.owner;
+    const newValue: string = req.body.newValue;
+    let findFlag: boolean = false;
+
+    posts.map(
+        (elem: post) => {
+            if (elem.owner === owner) {
+                findFlag = true;
+                elem.value = newValue;
+            }
+        }
+    )
+
+    if (findFlag) {
+        return res.status(200).json({
+            owner: owner,
+            newValue: newValue
+        });
+    }
+    else {
+        return res.status(400).send("Owner didn't find");
+    }
+}
+
+async function deletePostByOwner(req: Request, res: Response, next: NextFunction) {
+    const owner: string = req.body.owner;
+
+    posts = posts.filter(
+        (elem: post) => {
+            return elem.owner != owner
+        }
+    )
+
+    return res.sendStatus(200);
+}
+
+
+
+
+export default { getPosts, addPost, updatePostByOwner, deletePostByOwner };
